2502f4ba89e8fc39910bdaced72eafedd13d8946
qianqian121 <qianqian141@hotmail.com>
2016-07-31 10:05:52 -0700

    User can share a link to their friends or email it to themselves
    User can share a link to their friends or email it to themselves

--- app/src/main/java/com/codepath/nytimessearch/activities/ArticleActivity.java
+++ app/src/main/java/com/codepath/nytimessearch/activities/ArticleActivity.java
@@ -1,8 +1,13 @@
 package com.codepath.nytimessearch.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -18,6 +23,7 @@ public class ArticleActivity extends AppCompatActivity {
     @BindView(R.id.wvArticle)
     WebView wvArticle;
 
+    String url;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,7 +32,7 @@ public class ArticleActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        String url = getIntent().getStringExtra("webUrl");
+        url = getIntent().getStringExtra("webUrl");
         wvArticle.setWebViewClient(new WebViewClient() {
             /**
              * @param view
@@ -42,4 +48,32 @@ public class ArticleActivity extends AppCompatActivity {
         wvArticle.loadUrl(url);
     }
 
+    private ShareActionProvider mShareActionProvider;
+
+    private Intent getShareIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_TEXT, url);
+        shareIntent.setType("text/plain");
+        return shareIntent;
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_article, menu);
+        MenuItem sharedItem = menu.findItem(R.id.miShare);
+        mShareActionProvider = (ShareActionProvider)
+                MenuItemCompat.getActionProvider(sharedItem);
+        mShareActionProvider.setShareIntent(getShareIntent());
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        return super.onOptionsItemSelected(item);
+    }
 }
